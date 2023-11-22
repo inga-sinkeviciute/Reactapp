4e1b7bc00e14b3e29a7ffa3cb62a11ceaa05ac78
Inga Sinkeviciute <isinkeviciute7@gmail.com>
2023-11-22 20:45:24 +0200

    Header fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,14 @@
 import style from "./Header.module.css";
 
-export function Header() {
+export function Header({ updateAsideVisibility }) {
+	function showAside() {
+		updateAsideVisibility(true);
+	}
+
 	return (
 		<header className={style.mainHeader}>
 			<div className={style.logo}>Task-46</div>
-			<button id="add_task" className={style.btn}>
+			<button onClick={showAside} className={style.btn}>
 				Add task
 			</button>
 		</header>
